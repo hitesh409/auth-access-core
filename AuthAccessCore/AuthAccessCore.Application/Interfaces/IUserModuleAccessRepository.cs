f86f56a23b4634c5a8667d1ee6a2b68a87d5cc36
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2026-01-20 17:52:29 +0530

    Application : resolve effective module permissions

---
+++ AuthAccessCore/AuthAccessCore.Application/Interfaces/IUserModuleAccessRepository.cs
@@ -0,0 +1,9 @@
+﻿using AuthAccessCore.Domain.Enums;
+
+namespace AuthAccessCore.Application.Interfaces
+{
+    public interface IUserModuleAccessRepository
+    {
+        public Permissions GetPermissions(Guid userId, Guid moduleId);
+    }
+}
---
+++ AuthAccessCore/AuthAccessCore.Application/Services/AccessResolver.cs
@@ -0,0 +1,25 @@
+﻿using AuthAccessCore.Application.Interfaces;
+using AuthAccessCore.Domain.Enums;
+
+namespace AuthAccessCore.Application.Services
+{
+    public class AccessResolver : IAccessResolver
+    {
+        private readonly IUserModuleAccessRepository _userModuleAccess;
+        private readonly IRoleModuleAccessRepository _roleModuleAccess;
+        public AccessResolver(IUserModuleAccessRepository userModuleAccess, IRoleModuleAccessRepository roleModuleAccess) 
+        { 
+            _userModuleAccess = userModuleAccess;    
+            _roleModuleAccess = roleModuleAccess;
+        }
+        public Permissions GetPermissions(Guid userId, Guid moduleId, Roles role)
+        {
+            Permissions userPermission = _userModuleAccess.GetPermissions(userId, moduleId);
+            if (userPermission != Permissions.None)
+                return userPermission;
+
+            Permissions RolePermission = _roleModuleAccess.GetPermissions(role, moduleId);
+            return RolePermission;
+        }
+    }
+}
