bdb956ad1235a4a6fc92f7815355d22fac718f01
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2026-05-04 11:54:16 +0530

    feat(auth): add core auth services

---
+++ Frontend/AuthAccessUI/src/app/core/auth/permission.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+
+export class PermissionService{
+    private permissions = new Map<string,number>();
+
+    setPermissions(permissions: string[]){
+        this.permissions.clear();
+        permissions.forEach(p=>{
+            const [module, value] = p.split(':');
+            this.permissions.set(module,Number(value));
+        });
+    }
+
+    hasPermission(module: string, required: number): boolean{
+        const value = this.permissions.get(module);
+        if(value === undefined) return false;
+        return (value & required) === required;
+    }
+}
