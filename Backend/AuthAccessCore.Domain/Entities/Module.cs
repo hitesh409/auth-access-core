79321a1645278524060e77099e0c83730d55f527
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2026-05-12 21:32:55 +0530

    feat(Module): fix module parent relationship

--- Backend/AuthAccessCore.Domain/Entities/Module.cs
+++ Backend/AuthAccessCore.Domain/Entities/Module.cs
@@ -7,8 +7,11 @@ namespace AuthAccessCore.Domain.Entities
         public Guid ModuleId { get; private set; }
         public string ModuleName { get; private set; }
         public Permissions AllowedPermissions { get; private set; }
+        public Guid? ParentId { get; private set; }
+        public Module? Parent { get; private set; }
 
         // Navigation
+        public ICollection<Module> ChildModules { get; private set; } = new List<Module>();
         public ICollection<UserModuleAccess> UserModuleAccesses { get; private set; } = new List<UserModuleAccess>();
         public ICollection<RoleModuleAccess> RoleModuleAccesses { get; private set; } = new List<RoleModuleAccess>();
 
--- Backend/AuthAccessCore.Infrastructure/Migrations/20260512155936_FixedModuleParentRelationship.Designer.cs
+++ Backend/AuthAccessCore.Infrastructure/Migrations/20260512155936_FixedModuleParentRelationship.Designer.cs
@@ -4,6 +4,7 @@ using AuthAccessCore.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthAccessCore.Infrastructure.Migrations
 {
     [DbContext(typeof(AuthAccessDbContext))]
-    partial class AuthAccessDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260512155936_FixedModuleParentRelationship")]
+    partial class FixedModuleParentRelationship
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
