cf85e0716c76f975d6910623b8f2757ddf4de16f
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2026-05-13 22:15:15 +0530

    feat(auth-UI): add auth layout to embed auth related screens

--- Frontend/AuthAccessUI/src/app/app.routes.ts
+++ Frontend/AuthAccessUI/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //layout for auth related pages
+    {
+        path: '',
+        loadComponent : () => import('./features/auth/auth-layout/auth-layout').then(m => m.AuthLayout),
+        children:[
+            {
+                path: 'login',
+                loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
+            },
+            
+        ]
+    },
+
+    // protected routes
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard').then(m => m.Dashboard)
+    },
+
+    // default route
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard'
+    },
+    // page not found route
+    {
+        path: 'page-not-found',
+        loadComponent: () => import('./features/page-not-found/page-not-found').then(m => m.PageNotFound)
+    },
+    // wildcard route
+    {
+        path: '**',
+        redirectTo: 'page-not-found'
+    }
+];
--- Frontend/AuthAccessUI/src/app/app.ts
+++ Frontend/AuthAccessUI/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from "./features/auth/login/login";
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faEye, faEyeSlash, faKey, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
