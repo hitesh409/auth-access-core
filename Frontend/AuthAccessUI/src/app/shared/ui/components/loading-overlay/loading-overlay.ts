a507b4a5d99982af75b2b8ba3978942c97662431
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2026-06-01 22:42:39 +0530

    feat(ui): add enterprise loading overlay component

--- Frontend/AuthAccessUI/src/app/shared/ui/components/loading-overlay/loading-overlay.ts
+++ Frontend/AuthAccessUI/src/app/shared/ui/components/loading-overlay/loading-overlay.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-overlay',
   imports: [],
+  standalone: true,
   templateUrl: './loading-overlay.html',
   styleUrl: './loading-overlay.scss',
 })
-export class LoadingOverlay {}
+export class LoadingOverlay {
+  loading = input<boolean>(false);
+  message = input<string>('Loading...');
+}
